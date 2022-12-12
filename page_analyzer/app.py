d92b4a5fdb1fd3bc9e74d0f975b90a0e759264d2
mym1chelle <timur.a.samusenko@mail.ru>
2022-12-12 16:05:45 +0400

    Created a page to check the site with a table of checks

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, url_for
 import psycopg2
 import validators
 from datetime import datetime
@@ -15,7 +15,20 @@ cur.execute(
     CREATE TABLE IF NOT EXISTS urls (
         id serial PRIMARY KEY,
         name varchar UNIQUE,
-        created_at timestamp
+        created_at date
+        );"""
+    )
+
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS url_checks (
+        id serial PRIMARY KEY,
+        url_id bigint REFERENCES urls (id) ON DELETE CASCADE,
+        status_code integer,
+        h1 varchar,
+        title varchar,
+        description text,
+        created_at date
         );"""
     )
 conn.commit()
@@ -23,9 +36,6 @@ conn.commit()
 
 with open('database.sql', 'w') as file:
     cur.copy_to(file, 'urls', '|')
-
-# cur.close()
-# conn.close()
 
 
 @app.route('/')
@@ -66,17 +76,46 @@ def add_url():
     else:
         cur.execute("SELECT * FROM urls ORDER BY created_at DESC;")
         urls = cur.fetchall()
+        cur.execute(
+            """
+            SELECT created_at FROM url_checks
+            ORDER BY created_at DESC LIMIT 1;
+            """
+            )
+        date_last_check = cur.fetchall()[0][0]
+        print(date_last_check)
         return render_template(
             "sites_table.html",
-            urls_data=urls
+            urls_data=urls,
+            date_last_check=date_last_check
         )
 
 
 @app.route('/urls/<id>')
 def show_url(id):
     cur.execute("SELECT * FROM urls WHERE id=(%s);", (id,))
-    url = cur.fetchall()
+    url = cur.fetchall()[0]
+    cur.execute(
+        """
+        SELECT * FROM url_checks
+        WHERE url_id=(%s) ORDER BY created_at DESC;
+        """, (id,))
+    checks = cur.fetchall()
     return render_template(
         "urls.html",
-        url_data=url
+        url_data=url,
+        checks_url=checks
     )
+
+
+@app.post('/urls/<id>/checks')
+def check_url(id):
+    created_at = datetime.now()
+    cur.execute(
+        """
+        INSERT INTO url_checks (url_id, created_at)
+        VALUES (%s, %s);""", (id, created_at))
+    conn.commit()
+    with open('database.sql', 'w') as file:
+        cur.copy_to(file, 'urls', '|')
+    return redirect(url_for('show_url', id=id))
