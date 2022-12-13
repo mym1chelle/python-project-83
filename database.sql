d3648f737b04558fc824aaef3a8766db01e710ea
mym1chelle <timur.a.samusenko@mail.ru>
2022-12-13 09:28:11 +0400

    Made a basic template for the pages. Developed a function for parsing sites and adding information to the database

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-1|https://google.com|2022-12-12
-2|http://timursamusenko.com|2022-12-12
-3|http://sampo.ru|2022-12-12
-4|http://cat.tv|2022-12-12
+1|https://knitting-life.ru|2022-12-13
+2|https://google.com|2022-12-13
+3|https://apple.com|2022-12-13
+4|http://timursamusenko.com|2022-12-13
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,6 +4,7 @@ import validators
 from datetime import datetime
 import os
 import requests
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -92,8 +93,9 @@ def add_url():
             ON sorted_checks.url_id = dist_urls.id) as result;
             """
             )
-        last_check = cur.fetchall()
-        print(last_check)
+        last_check = [
+            ['' if x is None else x for x in row]
+            for row in cur.fetchall()]
         return render_template(
             "sites_table.html",
             last_check=last_check,
@@ -123,15 +125,28 @@ def check_url(id):
     cur.execute("SELECT name FROM urls WHERE id=(%s);", (id,))
     url = cur.fetchall()[0][0]
     try:
+        title = ''
+        h1 = ''
+        description = ''
         r = requests.get(url)
         status_code = r.status_code
+        html_doc = r.text
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        if soup.title:
+            title = soup.title.string
+        if soup.find('h1'):
+            h1 = soup.find('h1').string
+        if soup.find("meta", {"name": "description"}):
+            description = soup.find("meta", {"name": "description"})['content']
         cur.execute(
             """
-            INSERT INTO url_checks (url_id, status_code, created_at)
-            VALUES (%s, %s, %s);""", (id, status_code, created_at))
+            INSERT INTO url_checks (
+                url_id, status_code, h1, title, description, created_at)
+            VALUES (%s, %s, %s, %s, %s, %s);""",
+            (id, status_code, h1, title, description, created_at))
         conn.commit()
         with open('database.sql', 'w') as file:
-            cur.copy_to(file, 'urls', '|')
+            cur.copy_to(file, 'url_checks', '|')
     except requests.exceptions.ConnectionError:
         flash('Произошла ошибка при проверке', 'danger')
     return redirect(url_for('show_url', id=id))
