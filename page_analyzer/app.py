6585ed28f3f909b150913e30b0e44a96b6bde15d
mym1chelle <timur.a.samusenko@mail.ru>
2022-12-13 12:32:54 +0400

    Improved interaction with the database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 import psycopg2
+from psycopg2 import Error
+from psycopg2.extras import NamedTupleCursor
 import validators
 from datetime import datetime
 import os
@@ -10,29 +12,34 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
-cur = conn.cursor()
-cur.execute(
-    """
-    CREATE TABLE IF NOT EXISTS urls (
-        id serial PRIMARY KEY,
-        name varchar UNIQUE,
-        created_at date
-        );"""
-    )
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+except (Exception, Error) as error:
+    print('Can`t establish connection to database', error)
 
-cur.execute(
-    """
-    CREATE TABLE IF NOT EXISTS url_checks (
-        id serial PRIMARY KEY,
-        url_id bigint REFERENCES urls (id) ON DELETE CASCADE,
-        status_code integer,
-        h1 varchar,
-        title varchar,
-        description text,
-        created_at date
-        );"""
-    )
+
+with conn.cursor() as cur:
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS urls (
+            id serial PRIMARY KEY,
+            name varchar UNIQUE,
+            created_at date
+            );"""
+        )
+with conn.cursor() as cur:
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS url_checks (
+            id serial PRIMARY KEY,
+            url_id bigint REFERENCES urls (id) ON DELETE CASCADE,
+            status_code integer,
+            h1 varchar,
+            title varchar,
+            description text,
+            created_at date
+            );"""
+        )
 conn.commit()
 
 
@@ -49,65 +56,61 @@ def add_url():
         data = request.form.to_dict()
         url = data.get('url')
         if validators.url(url):
-            print('True')
             created_at = datetime.now()
-            print(created_at)
-            cur.execute("SELECT name FROM urls WHERE name=(%s);", (url,))
-            get_url = cur.fetchone()
-            if get_url is None:
-                cur.execute(
-                    """
-                    INSERT INTO urls (name, created_at)
-                    VALUES (%s, %s);""", (url, created_at))
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute("SELECT id FROM urls WHERE name=(%s);", (url,))
+                url_id = cur.fetchone()
+            if url_id is None:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        INSERT INTO urls (name, created_at)
+                        VALUES (%s, %s);""", (url, created_at))
                 conn.commit()
                 flash('Страница успешно добавлена', 'success')
-                with open('database.sql', 'w') as file:
-                    cur.copy_to(file, 'urls', ',')
             else:
                 flash('Страница уже существует', 'info')
-                cur.execute("SELECT id FROM urls WHERE name=(%s);", (url,))
-                redirect(f'urls/{cur.fetchone()[0]}')
+                redirect(f'urls/{url_id.id}')
         else:
             flash('Некорректный URL', 'danger')
             return render_template('start_page.html', url_adress=url)
-        cur.execute("SELECT id FROM urls WHERE name=(%s);", (url,))
-        return redirect(f'urls/{cur.fetchone()[0]}')
     else:
-        cur.execute(
-            """
-            SELECT *
-            FROM (
-            SELECT DISTINCT ON(dist_urls.id)
-            dist_urls.id, dist_urls.name,
-            sorted_checks.status_code, sorted_checks.created_at
-            FROM (SELECT * FROM urls ORDER BY created_at DESC) AS dist_urls
-            LEFT JOIN (
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+            cur.execute(
+                """
                 SELECT *
-                FROM url_checks
-                ORDER BY created_at DESC
-                ) AS sorted_checks
-            ON sorted_checks.url_id = dist_urls.id) as result;
-            """
-            )
-        last_check = [
-            ['' if x is None else x for x in row]
-            for row in cur.fetchall()]
+                FROM (
+                SELECT DISTINCT ON(dist_urls.id)
+                dist_urls.id, dist_urls.name,
+                sorted_checks.status_code, sorted_checks.created_at
+                FROM (SELECT * FROM urls ORDER BY created_at DESC) AS dist_urls
+                LEFT JOIN (
+                    SELECT *
+                    FROM url_checks
+                    ORDER BY created_at DESC
+                    ) AS sorted_checks
+                ON sorted_checks.url_id = dist_urls.id) as result;
+                """
+                )
+            all_urls = cur.fetchall()
         return render_template(
             "sites_table.html",
-            last_check=last_check,
+            last_check=all_urls,
         )
 
 
 @app.route('/urls/<id>')
 def show_url(id):
-    cur.execute("SELECT * FROM urls WHERE id=(%s);", (id,))
-    url = cur.fetchall()[0]
-    cur.execute(
-        """
-        SELECT * FROM url_checks
-        WHERE url_id=(%s) ORDER BY id DESC;
-        """, (id,))
-    checks = cur.fetchall()
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute("SELECT * FROM urls WHERE id=(%s);", (id,))
+        url = cur.fetchone()
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute(
+            """
+            SELECT * FROM url_checks
+            WHERE url_id=(%s) ORDER BY id DESC;
+            """, (id,))
+        checks = cur.fetchall()
     return render_template(
         "urls.html",
         url_data=url,
@@ -118,13 +121,14 @@ def show_url(id):
 @app.post('/urls/<id>/checks')
 def check_url(id):
     created_at = datetime.now()
-    cur.execute("SELECT name FROM urls WHERE id=(%s);", (id,))
-    url = cur.fetchall()[0][0]
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        cur.execute("SELECT name FROM urls WHERE id=(%s);", (id,))
+        url_name = cur.fetchone()
     try:
         title = ''
         h1 = ''
         description = ''
-        r = requests.get(url)
+        r = requests.get(url_name.name)
         status_code = r.status_code
         html_doc = r.text
         soup = BeautifulSoup(html_doc, 'html.parser')
@@ -134,12 +138,13 @@ def check_url(id):
             h1 = soup.find('h1').string
         if soup.find("meta", {"name": "description"}):
             description = soup.find("meta", {"name": "description"})['content']
-        cur.execute(
-            """
-            INSERT INTO url_checks (
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO url_checks (
                 url_id, status_code, h1, title, description, created_at)
-            VALUES (%s, %s, %s, %s, %s, %s);""",
-            (id, status_code, h1, title, description, created_at))
+                VALUES (%s, %s, %s, %s, %s, %s);""",
+                (id, status_code, h1, title, description, created_at))
         conn.commit()
     except requests.exceptions.ConnectionError:
         flash('Произошла ошибка при проверке', 'danger')
